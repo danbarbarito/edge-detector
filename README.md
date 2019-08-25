283e4a2c723ac1d1a9b850c23d4dcbeb6dd1f044
Dan Barbarito <dan@barbarito.me>
2019-08-25 11:06:20 -0400

    sfml and readme

---
+++ README.md
@@ -0,0 +1,10 @@
+# Building
+
+1. `cmake .`
+2. `make`
+3. `./edge-detection`
+
+# Dependencies
+
+1. Boost
+2. SFML 2.5
--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,31 @@
-// SDL2 Hello, World!
-// This should display a white screen for 2 seconds
-// compile with: clang++ main.cpp -o hello_sdl2 -lSDL2
-// run with: ./hello_sdl2
-#include <SDL2/SDL.h>
-#include <stdio.h>
+#include <SFML/Graphics.hpp>
 
-#define SCREEN_WIDTH 640
-#define SCREEN_HEIGHT 480
+int main()
+{
+  // create the window
+  sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
 
-int main(int argc, char* args[]) {
-  SDL_Window* window = NULL;
-  SDL_Surface* screenSurface = NULL;
-  if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-    fprintf(stderr, "could not initialize sdl2: %s\n", SDL_GetError());
-    return 1;
+  // run the program as long as the window is open
+  while (window.isOpen())
+  {
+    // check all the window's events that were triggered since the last iteration of the loop
+    sf::Event event;
+    while (window.pollEvent(event))
+    {
+      // "close requested" event: we close the window
+      if (event.type == sf::Event::Closed)
+        window.close();
+    }
+
+    // clear the window with black color
+    window.clear(sf::Color::Black);
+
+    // draw everything here...
+    // window.draw(...);
+
+    // end the current frame
+    window.display();
   }
-  window = SDL_CreateWindow(
-			    "hello_sdl2",
-			    SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-			    SCREEN_WIDTH, SCREEN_HEIGHT,
-			    SDL_WINDOW_SHOWN
-			    );
-  if (window == NULL) {
-    fprintf(stderr, "could not create window: %s\n", SDL_GetError());
-    return 1;
-  }
-  screenSurface = SDL_GetWindowSurface(window);
-  SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
-  SDL_UpdateWindowSurface(window);
-  SDL_Delay(2000);
-  SDL_DestroyWindow(window);
-  SDL_Quit();
+
   return 0;
 }
